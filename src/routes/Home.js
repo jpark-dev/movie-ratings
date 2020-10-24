388ffe4dd6e1a8154ccce0213be900e2a52c4b17
Jasom Park <zeipar@gmail.com>
2020-10-23 23:22:43 -0700

    Gave styling using styled-components.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIES = gql`
   {
@@ -10,17 +11,50 @@ const GET_MOVIES = gql`
   }
 `;
 
-console.log(GET_MOVIES);
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 export default () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map(movie => <h1>{movie.id}</h1>);
-  }
-  if (error) {
-    return error;
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Movie Ratings</Title>
+        <Subtitle>With GraphQL!!!</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+    </Container>
+  );
 };
