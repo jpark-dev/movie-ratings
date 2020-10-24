95e915d00c6bb7cca56269542cec51f457dfe732
Jasom Park <zeipar@gmail.com>
2020-10-23 23:40:47 -0700

    Implemented useParam hook to obtain an id.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,8 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default () => {
+  const { id } = useParams();
+  console.log(id);
+  return "Detail";
+};
