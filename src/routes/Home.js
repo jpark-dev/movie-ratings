60d86500270328abc87a014465ac264776d95c2c
Jasom Park <zeipar@gmail.com>
2020-10-23 23:07:17 -0700

    Used movie API app built previously, running at port 4000.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,19 @@
-export default () => "Home";
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+console.log(GET_MOVIES);
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  return <h1>Home</h1>;
+};
