eae84efb8fc40dca79bc3fd45021191cea3dc9fc
Jasom Park <zeipar@gmail.com>
2020-10-24 18:14:56 -0700

    Used useQuery to fetch data from movieAPI, and then to show the title if exists.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 export default () => {
   const { id } = useParams();
-  console.log(id);
-  return "Detail";
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
