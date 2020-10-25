917b63f59759ae108ba9f14e73dd82a392a729f5
Jasom Park <zeipar@gmail.com>
2020-10-24 18:23:49 -0700

    Added styling with components.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -14,15 +15,54 @@ const GET_MOVIE = gql`
   }
 `;
 
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
+`;
+
 export default () => {
   const { id } = useParams();
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: parseInt(id) },
   });
-  if (loading) {
-    return "loading";
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+
+  return (
+    <Container>
+      <Column>
+        <Title>Title</Title>
+        <Subtitle>Subt</Subtitle>
+        <Description>Desc</Description>
+      </Column>
+      <Poster>Image here</Poster>
+    </Container>
+  );
 };
